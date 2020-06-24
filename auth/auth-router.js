55aed260a8ea6569599e8eb98c97aa83b2f810af
Seth Nelson <seth.nelson@outlook.com>
2020-06-24 15:16:45 -0700

    added users and config folder for router and secrets, added language to all auth files

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,65 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+const bcryptjs = require('bcryptjs');
+const secrets = require('../config/secrets.js');
+
+const Users = require('../users/users-model.js');
+const { isValid } = require('../users/users-service.js');
+
+
+function generateToken(user) {
+  const payload = {
+    userId: user.id,
+    username: user.username,
+  };
+    const options = {
+      expiresIn: '2H'
+    };
+
+    return jwt.sign(payload, secrets.jwtSecret, options)
+}
 
 router.post('/register', (req, res) => {
-  // implement registration
+  const credentials = req.body;
+
+  if (isValid(credentials)) {
+    const rounds = process.env.BCRYPT_ROUNDS || 7;
+    const hash = bcryptjs.hashSync(credentials.password, rounds);
+    credentials.password = hash;
+
+    Users.add(credentials)
+    .then(user => {
+      const token = genToken(saved);
+      res.status(201).json({ data: user, token });
+    })
+    .catch(err => {
+      res.status(500).json({ message: err.message });
+    });
+  } else {
+    res.status(400).json({ message: 'Please provide alphanumeric username and password.' });
+  }
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  const { username, password } = req.body;
+
+  if (isValid(req.body)) {
+    Users.findBy({ username: username })
+      .then(([user]) => {
+        if (user && bcryptjs.compareSync(password, user.password)) {
+          const token = generateToken(user);
+          res.status(200).json({ message: 'Welcome to our API.', token});
+        } else {
+          res.status(401).json({ message: 'Invalid credentials.' });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ message: error.message });
+      });
+    } else {
+      res.status(400).json({ message: 'Please provide alphanumerical username and password.' });
+    }
 });
+
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,25 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets.js');
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  try {
+    const token = req.headers.authorization.split('')[1];
+    if (token) {
+      jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+        if (err) {
+          res.status(401).json({ errorMessage: 'Token is not valid.' }); 
+        } else {
+          req.decodedToken = decodedToken;
+          next()
+        }
+      })
+    } else {
+      throw new Error('invalid auth data');
+    }
+  } catch (err) {
+    res.status(401).json({ error: err.message });
+  }
 };
